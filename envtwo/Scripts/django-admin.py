dda5879e0314933a46f2575d135ac4485b1f1184
Mercykylax <nyawiramercy49@gmail.com>
2019-05-14 22:24:48 +0300

    project 2

---
+++ envtwo/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\student.akhp003.001\desktop\python class\django-project2\envtwo\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
